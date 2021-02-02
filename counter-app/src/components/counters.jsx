f04d8197ca1f9667bf7112ed83b207a51c6d95fb
Fojan Babaali <fozhanmail@gmail.com>
2021-02-01 23:18:39 -0800

    Update counters.jsx

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -8,8 +8,13 @@ class Counters extends Component {
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
-    ],
+    ]
   };
+
+  handleDelete = (counterId) => {
+    const newCounter = this.state.counters.filter(c => c.id !==counterId);
+    this.setState({counters:newCounter});
+  }
 
   render() {
     return (
@@ -22,7 +27,7 @@ class Counters extends Component {
               padding: "20px",
             }}
           >
-            <Counter key={counter.id}  value={counter.value} id={counter.id} onDelete ={this.handleDelete}>
+            <Counter key={counter.id}  value={counter.value} id={counter.id} onDelete={this.handleDelete}>
               {/* Counter has these props as well as his own state */}
                 {/* <h3>Title</h3> */}
             </Counter>
@@ -40,10 +45,7 @@ class Counters extends Component {
   }
 
 
-  handleDelete = (counterId) => {
-    const newCounter = this.state.counters.filter(c => c.id !==counterId);
-    this.setState({counters:newCounter});
-  }
+  
   
 }
 
